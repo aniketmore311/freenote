d844ed6c9cbc4d3cb576d50bae026ea679d45c14
aniket <aniketavinashmore343@gmail.com>
2021-11-17 15:12:31 +0530

    change sha version

--- freenote-server/src/lib/password.ts
+++ freenote-server/src/lib/password.ts
@@ -7,14 +7,14 @@ export async function hashPassword(
 ): Promise<string> {
   const salt = crypto.randomBytes(16).toString('hex');
   const iterations = 100000;
-  const buff = await pbkdf2p(password, Buffer.from(salt, 'hex'), iterations, 32, "sha512");
+  const buff = await pbkdf2p(password, Buffer.from(salt, 'hex'), iterations, 32, "sha256");
   const hash = buff.toString('hex');
   const toStore = `${hash}:${salt}:${iterations}`;
   return toStore;
-};
+}
 
 export async function hashString(data: string, salt: string) {
-  const buff = await pbkdf2p(data, salt, 100000, 32, 'sha512');
+  const buff = await pbkdf2p(data, salt, 100000, 32, 'sha256');
   return buff.toString('hex');
 }
 
@@ -23,7 +23,7 @@ export async function match(password: string, hash: string) {
   const passhash = tokens[0];
   const salt = tokens[1];
   const iterations = parseInt(tokens[2]);
-  const buff = await pbkdf2p(password, Buffer.from(salt, 'hex'), iterations, 32, "sha512");
+  const buff = await pbkdf2p(password, Buffer.from(salt, 'hex'), iterations, 32, "sha256");
   const computedHash = buff.toString('hex');
   if (computedHash === passhash) {
     return true;
