eca785c8a4f056ec5b7c061013a91edc7596aaae
aniket <aniketavinashmore343@gmail.com>
2021-11-17 18:29:15 +0530

    env vars support

--- freenote-server/src/config/config.ts
+++ freenote-server/src/config/config.ts
@@ -6,6 +6,8 @@ export const config = {
     nodeEnv: getEnv('NODE_ENV', "development"),
     client: getEnv('CLIENT', "sqlite3"),
     dbUri: getEnv('DB_URI', "db.sqlite"),
-    secretKey: getEnv('SECRET_KEY', "secret_key")
+    secretKey: getEnv('SECRET_KEY', "secret_key"),
+    accessTokenExpStr: getEnv('ACCESS_TOKEN_EXP_STR', "15min"),
+    refreshTokenExpStr: getEnv('REFRESH_TOKEN_EXP_STR', "30d")
   },
 }
