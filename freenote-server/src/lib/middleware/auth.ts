766995e5c397378c0594fb3f25354c7284feb00b
aniket <aniketavinashmore343@gmail.com>
2021-11-17 18:29:39 +0530

    add options to auth

--- freenote-server/src/lib/middleware/auth.ts
+++ freenote-server/src/lib/middleware/auth.ts
@@ -1,10 +1,12 @@
 import { Handler } from "express";
 import jwt from 'jsonwebtoken';
 import { config } from "../../config/config";
+import { IUserToken, User } from "../../models/User";
 import { APIError } from "../APIError";
+import { catchAsync } from "../utils";
 
-export function auth(): Handler {
-  return function (req, res, next) {
+export function auth(opts?: { fetchUser: boolean }): Handler {
+  return catchAsync(async function (req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       throw APIError.badRequest('Authorization token missing');
@@ -15,10 +17,16 @@ export function auth(): Handler {
     }
     try {
       const userToken = jwt.verify(parts[1], config.env.secretKey);
-      res.locals.userToken = userToken;
+      req.userToken = userToken as IUserToken;
+      // res.locals.userToken = userToken;
+      if (opts && opts.fetchUser) {
+        const user = await User.query().findOne({ id: req.userToken.id, is_deleted: false });
+        req.user = user;
+      }
       next();
-    }catch(err){
+    } catch (err) {
       throw APIError.unauthorized('invalid Authorization token');
     }
-  }
+  })
+
 }
